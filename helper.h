69852e697224a7f547cbcb556fa95b1797b16eb7
Henrik Sandström I <henrik.i.sandstrom@ericsson.com>
2023-10-04 14:24:23 +0200

    update

--- helper.h
+++ helper.h
@@ -1,7 +1,6 @@
 #ifndef MY_PROJECT_HELPER_H
+#define MY_PROJECT_HELPER_H
 
-  #define MY_PROJECT_HELPER_H
-
-  void helper_function();  // Declaration of the function in helper.c
+void helper_function();  // Declaration of the function in helper.c
 
 #endif //MY_PROJECT_HELPER_H
